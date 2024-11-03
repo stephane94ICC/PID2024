c40c15f65b526cba36aa944176c40aeac4a6d298
stephane94ICC <lalala33u@outlook.com>
2024-11-03 20:53:38 +0100

    chap2Start

---
+++ src/main/java/com/icc/reservations_springboot/model/Artist.java
@@ -0,0 +1,54 @@
+package com.icc.reservations_springboot.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="artists")
+public class Artist {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String firstname;
+    private String lastname;
+
+    protected Artist() {}
+
+    public Artist(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String toString() {
+        return firstname + " " + lastname;
+    }
+
+}
