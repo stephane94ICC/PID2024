3982cb8057f07956c4cbc4e7f1b46c2d5afd607a
stephane94ICC <lalala33u@outlook.com>
2025-01-26 23:20:44 +0100

    Controller Show +templates +routes

---
+++ src/main/java/com/icc/reservations_springboot/controller/ShowController.java
@@ -0,0 +1,40 @@
+package com.icc.reservations_springboot.controller;
+
+import com.icc.reservations_springboot.model.Show;
+import com.icc.reservations_springboot.service.ShowService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+
+@Controller
+public class ShowController {
+    @Autowired
+    ShowService service;
+
+    @GetMapping("/shows")
+    public String index(Model model) {
+        List<Show> shows = service.getAll();
+
+        model.addAttribute("shows", shows);
+        model.addAttribute("title", "Liste des spectacles");
+
+        return "show/index";
+    }
+
+    @GetMapping("/shows/{id}")
+    public String show(Model model, @PathVariable("id") String id) {
+        Show show = service.get(id);
+
+        model.addAttribute("show", show);
+        model.addAttribute("title", "Fiche d'un spectacle");
+
+        return "show/show";
+    }
+
+}
+
