1ab6bb8941704a12c25511f668ece3557de48242
stephane94ICC <lalala33u@outlook.com>
2025-01-26 14:50:50 +0100

    error

--- src/main/java/com/icc/reservations_springboot/controller/InscriptionController.java
+++
@@ -1,9 +0,0 @@
-package com.icc.reservations_springboot.controller;
-
-
-import com.icc.reservations_springboot.model.UserDto;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.context.request.WebRequest;
-
-@GetMapping("/user/inscription")
