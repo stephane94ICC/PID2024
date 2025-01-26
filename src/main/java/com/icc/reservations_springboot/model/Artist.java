4253a3ba6e33dd92c69874d11561df710d5cefef
stephane94ICC <lalala33u@outlook.com>
2025-01-26 16:15:24 +0100

    Modèle Artist +migration +données initiales

--- src/main/java/com/icc/reservations_springboot/model/Artist.java
+++ src/main/java/com/icc/reservations_springboot/model/Artist.java
@@ -11,15 +11,18 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
-@NoArgsConstructor
-@Entity
-@Table(name = "artists")
-public class Artist {
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor(force = true, access = AccessLevel.PROTECTED)
+@Entity
+@Table(name="artists")
+public class Artist {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
     private String firstname;
     private String lastname;
+
 }
--- src/main/java/com/icc/reservations_springboot/repository/ArtistRepository.java
+++ src/main/java/com/icc/reservations_springboot/repository/ArtistRepository.java
@@ -6,9 +6,7 @@ import java.util.List;
 import com.icc.reservations_springboot.model.Artist;
 import org.springframework.data.repository.CrudRepository;
 
-
 public interface ArtistRepository extends CrudRepository<Artist, Long> {
-
     List<Artist> findByLastname(String lastname);
 
     Artist findById(long id);
