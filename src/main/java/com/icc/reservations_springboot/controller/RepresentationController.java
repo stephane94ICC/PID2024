f891caf7d6861e89ab96d6429dcd1f2a4af79ec6
stephane94ICC <lalala33u@outlook.com>
2025-01-27 21:37:37 +0100

    Controller Representation +templates +routes

---
+++ src/main/java/com/icc/reservations_springboot/controller/RepresentationController.java
@@ -0,0 +1,43 @@
+package com.icc.reservations_springboot.controller;
+
+import java.util.List;
+
+import com.icc.reservations_springboot.model.Representation;
+import com.icc.reservations_springboot.service.RepresentationService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+@Controller
+public class RepresentationController {
+    @Autowired
+    RepresentationService service;
+
+    @GetMapping("/representations")
+    public String index(Model model) {
+        List<Representation> representations = service.getAll();
+
+        model.addAttribute("representations", representations);
+        model.addAttribute("title", "Liste des representations");
+
+        return "representation/index";
+    }
+
+    @GetMapping("/representations/{id}")
+    public String show(Model model, @PathVariable("id") String id) {
+        Representation representation = service.get(id);
+
+        model.addAttribute("representation", representation);
+        model.addAttribute("date", representation.getWhen().toLocalDate());
+        model.addAttribute("heure", representation.getWhen().toLocalTime());
+        model.addAttribute("title", "Fiche d'une representation");
+
+        return "representation/show";
+    }
+
+}
