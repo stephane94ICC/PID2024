f6acfe8877a8287cbc6b5f519a2b60ce32ec6fd8
stephane94ICC <lalala33u@outlook.com>
2025-01-27 22:51:07 +0100

    Relation ManyToMany between User & Role

--- src/main/java/com/icc/reservations_springboot/model/Role.java
+++ src/main/java/com/icc/reservations_springboot/model/Role.java
@@ -1,10 +1,9 @@
 package com.icc.reservations_springboot.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -15,10 +14,16 @@ public class Role {
     private Long id;
     private String role;
 
-    protected Role() {	}
+    @ManyToMany
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<User> users = new ArrayList<>();
+
+    protected Role() { }
 
     public Role(String role) {
-        super();
         this.role = role;
     }
 
@@ -32,6 +37,28 @@ public class Role {
 
     public void setRole(String role) {
         this.role = role;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public Role addUser(User user) {
+        if(!this.users.contains(user)) {
+            this.users.add(user);
+            user.addRole(this);
+        }
+
+        return this;
+    }
+
+    public Role removeUser(User user) {
+        if(this.users.contains(user)) {
+            this.users.remove(user);
+            user.getRoles().remove(this);
+        }
+
+        return this;
     }
 
     @Override
--- src/main/java/com/icc/reservations_springboot/model/User.java
+++ src/main/java/com/icc/reservations_springboot/model/User.java
@@ -1,18 +1,16 @@
 package com.icc.reservations_springboot.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="users")
 public class User {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
     private String login;
     private String password;
@@ -20,16 +18,17 @@ public class User {
     private String lastname;
     private String email;
     private String langue;
-    private String role;
     private LocalDateTime created_at;
+
+    @ManyToMany(mappedBy = "users")
+    private List<Role> roles = new ArrayList<>();
 
     protected User() {}
 
-    public User(String login, String firstname, String lastname, String role) {
+    public User(String login, String firstname, String lastname) {
         this.login = login;
         this.firstname = firstname;
         this.lastname = lastname;
-        this.role = role;
         this.created_at = LocalDateTime.now();
     }
 
@@ -85,20 +84,38 @@ public class User {
         this.langue = langue;
     }
 
-    public String getRole() {
-        return langue;
+    public List<Role> getRoles() {
+        return roles;
     }
 
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public LocalDateTime getCreatedAt() {
+    public LocalDateTime getCreated_at() {
         return created_at;
+    }
+
+    public User addRole(Role role) {
+        if(!this.roles.contains(role)) {
+            this.roles.add(role);
+            role.addUser(this);
+        }
+
+        return this;
+    }
+
+    public User removeRole(Role role) {
+        if(this.roles.contains(role)) {
+            this.roles.remove(role);
+            role.getUsers().remove(this);
+        }
+
+        return this;
     }
 
     @Override
     public String toString() {
-        return login + "(" + firstname + " " + lastname + " - " + role + ")";
+        return login + "(" + firstname + " " + lastname + ")";
+    }
+
+    public String getRole() {
+        return "";
     }
 }
